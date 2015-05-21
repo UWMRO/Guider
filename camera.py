3de0b0dadcfb04f1b87dec0fcdf00e1991bb2322
Joseph Huehnerhoff <jwhueh@gmail.com>
2015-05-21 07:20:45 -0600

    classify camera.py

--- camera.py
+++ camera.py
@@ -1,44 +1,63 @@
-# Camera.py
-# Camera imaging and fits routines using input from camera.cpp
+#! /usr/bin/python
+"""
+ Camera.py
+Camera imaging and fits routines using input from camera.cpp
+"""
 
 import numpy as np
 import pyfits
 import subprocess
 import time
 
-# =========================================
-# Run camera routine
-# =========================================
-name = "basic"
-expose = "4000"
+class CameraExpose(object):
+    def __init__(self, name, exp):
+        self.name = name
+        self.exp = exp
+        self.wait = 3.0
 
-# Tells camera to take an image, it will output a binary file named "test" with 1000 ms exposure.
-# Can also use './camera test 0 0' to check camera.
-subprocess.Popen(['/home/linaro/Camera/camera', 'image', 'binary', expose])
+    def expose(self):
+        try:
+            # =========================================
+            # Run camera routine
+            # =========================================
+            #name = "basic"
+            #expose = "4000"
 
-# Pause for the camera to run
-time.sleep(3+(int(expose)/1000))
+            # Tells camera to take an image, it will output a binary file named "test" with 1000 ms exposure.
+            # Can also use './camera test 0 0' to check camera.
+            subprocess.Popen(['/home/linaro/Camera/camera', 'image', 'binary', self.exp])
 
-#==============================================
-# Open binary file from camera as a numpy array
-# =============================================
-binary=np.fromfile('binary',dtype='u1').reshape(1280,1024)
+            # Pause for the camera to run
+            time.sleep(self.wait+(int(expose)/1000))
 
-# --------------------------
-# Used for testing array procedure, can remove once program is tested on-sky.
-# print binary.shape
-# print binary.dtype.name
-# print binary
-# ---------------------------
+            #==============================================
+            # Open binary file from camera as a numpy array
+            # =============================================
+            binary=np.fromfile('binary',dtype='u1').reshape(1280,1024)
 
-# Create a primary header file for the FITS image
-hdu=pyfits.PrimaryHDU(binary)
-hdulist=pyfits.HDUList([hdu])
+            # --------------------------
+            # Used for testing array procedure, can remove once program is tested on-sky.
+            # print binary.shape
+            # print binary.dtype.name
+            # print binary
+            # ---------------------------
 
-# Add time, exposure length, and file name to header.
-# Need to add
+            # Create a primary header file for the FITS image
+            hdu=pyfits.PrimaryHDU(binary)
+            hdulist=pyfits.HDUList([hdu])
 
-# Write the image and header to a FITS file using variable name.
-hdulist.writeto(name+'.fits')
+            # Add time, exposure length, and file name to header.
+            # Need to add
 
-print "Camera and FITS routines complete" 
+            # Write the image and header to a FITS file using variable name.
+            hdulist.writeto(name+'.fits')
+
+            print "Camera and FITS routines complete" 
+            return 1
+        except:
+            print "failed"
+            return 0
+
+    def status(self):
+        print "return some status message"
+        return 0
