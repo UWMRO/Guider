7fddde81651036701944b4d622d6689641d31bc9
Joseph Huehnerhoff <jwhueh@gmail.com>
2015-06-27 00:26:32 +0000

    more comments

--- Guider.py
+++ Guider.py
@@ -44,39 +44,49 @@ class Guider(object):
         self.l = Logger() #Logger class creates logfile of processes
         self.fakeImageDir = '/Users/jwhueh/projects/MRO/guiding_images/gcam_UT150425/' # Directory where fake images are
 										       # stored for testing. 
-        self.fakeOut =  False #
-        self.currentImage = 2 #?
-        self.logType = 'guider' #parameter for Logger class?
-        self.thres = 30 #threshold to match coordinates, to find ref star with coordCompare
-	self.takeRef = False #variable can be set to True to get a new reference image taken
+        self.fakeOut =  False # Boolean value that if set to True, will test code using
+			      # a set of test images, rather than taking new images
+        self.currentImage = 2 # Integer used in testing, tracks how many images have been taken
+        self.logType = 'guider' # Parameter used in  Logger class to create logfile
+        self.thres = 30 # Thresholc value used in coordCompare when matching coordinates of
+			# objects in images
+	self.takeRef = False # Boolean value set to tell code to take a new reference image.
+			     # Can be changed via the setTakeRef function.
 
 # Takes the class, a string keyword for image type, a string for image
 # name, an integer for exposure tiem, and a string for directory name.
 # The function checks if the variable fakeOut is equal to true first
 # -- if so, ends function and carries out rest of code using existing
-# images. If not, the function takes an image using the CameraExpose
+# images.  If not, the function takes an image using the CameraExpose
 # object defined in the constructor, and checks that the image has
 # been taken and saved.
     def takeImage(self, imType = None, imgName = None, imExp = None, imDir = None): 
         if self.fakeOut != True: 
             im = self.c.runExpose(imgName, imExp, imDir)
             self.l.logStr('Image\t%s %s %s' % (str(imgName), str(imExp), str(imDir)), self.logType)
-            if im == True: #check on completion and save of image exposure
+            if im == True: # check on completion and save of image exposure
                 return
             else:
-                raise Exception
+                raise Exception("Image exposure not completed") 
         else:
-            return 3
+            return 3 # Simply returns if no exception raised
 
-    #uses PyGuide function findStars on iamge to get coordinates of all stars in field, stores 
-    #coordinates in an array
+# Takes the class, and a string image name as parameters. The
+# function then creates an array to store the object ccoordinates
+# in, opens the image with the passed name, and takes the data
+# (0th layer) from the image.  The function then uses PyGuide's
+# (written by Russel Owen) function findStars to find the
+# stars in the image.  The output of findStars is then iterated
+# through, and x and y coordinates are pulled out and used along 
+# with star names to populate the coorinate list 'output',
+# which is then returned.
     def analyze(self,im): 
         output=[]
         hdulist = fits.open(im)
         data = hdulist[0].data
 
-        ccd = PyGuide.CCDInfo(200,21.3,1.6) #Since we're using it on one CCD, these should be constants
-        ctr,imstat = PyGuide.findStars(data,None,None,ccd,1,False)[0:2] #need to choose mask here
+        ccd = PyGuide.CCDInfo(200,21.3,1.6) # Since we're using it on one CCD, these should be constants
+        ctr,imstat = PyGuide.findStars(data,None,None,ccd,1,False)[0:2]
 
         size= len(ctr)
         xcoord = ['XCoord']
@@ -92,9 +102,14 @@ class Guider(object):
             output.append(star_out)
         return output
 
-    #Takes the coordinate array from analyze and finds the difference between these coordinates
-    #and those for the reference image, to get x and y offsets to give to the telescope
-		#do we need to do anything here to convert this to RA and dec?
+# Takes the class and the coordinate array from the analyze 
+# function as parameters and finds the difference between these 
+# coordinates and those for the reference image, to get x and 
+# y offsets to give to the telescope. Before the offsets in x 
+# and y are returned, runs a check to ensure that the offsets 
+# are not too large (greater than some threshold defined 
+# globally).  If they are, sets self.quit to True and writes 
+# an error message to the console.
     def getOffset(self, imCoords):
         self.l.logStr('OffsetInput\t'+str(self.ref)+'\t'+str(imCoords), self.logType)
         x = self.ref[1]
@@ -110,8 +125,14 @@ class Guider(object):
         return xoff, yoff
 
    
-#goes through coordinate list for current image and finds new coordinates of guide star (chosen in ref image)
-#by comparing coordinates and matching within some given threshole
+# Takes the class, the coordinate list generated from the reference 
+# image, the coordinate list generated from the most recent tracking
+# image, as well as the globally defined offset threshold as parameters.
+# The function then goes through coordinate lists for current image and 
+# finds new coordinates of guide star (chosen in ref image) by comparing
+# coordinates and matching within some given threshold. If found within
+# the threshold, returns true.  If not, sets self.quit to True to stop
+# guiding operatinos and prints an error message to the console.
      def coordCompare(self, c0, c1, thres): 
         if np.abs(c0[1] - c1[1]) > float(thres) and np.abs(c0[2] - c1[2]) > float(thres):
             self.quit = True
@@ -119,6 +140,8 @@ class Guider(object):
         else:
             return True
 
+# This function takes only the class as a parameter, and is used for
+# testing purposes.
     def test(self):
 	self.refName = time.strftime("%Y%m%dT%H%M%S") + ".fits"
         self.takeImage('image', self.refName,self.expTime, None)
@@ -126,28 +149,42 @@ class Guider(object):
 	print self.analyze(self.refName)
 	return
 
-#takes a new reference image. This the coordinate of objects in this image will serve as the target for guiding
-#this function creates a name for the reference image and saves it, takes the image, analyzes it, and saves the
-#coordinate list for that image. It then sets the guide star to be the star at the 0th spot in the coordinate array
+# Takes only the class as a parameter.  This function takes a new 
+# reference image, the coordinate of objects in which  will 
+# serve as the target for guiding.  This function creates a name 
+# for the reference image and saves it, takes the image, analyzes
+# it, and globally saves the coordinate list for that image.  It 
+# then sets the guide star to be the star at the 0th spot in the 
+# coordinate array (the brightest star), and sets the coordinates
+# of it to the variable name self.ref.
     def takeRef(self):
 	self.refName = time.strftime("%Y%m%dT%H%M%S") + ".fits"
-        if self.fakeOut == True: #if has been told to guide on fake data that already exists in the directory, goes into this loop
-            self.refName = self.fakeImageDir+'g' + str(self.currentImage).zfill(4)+'.fits'
-            self.currentImage = self.currentImage +1
+        if self.fakeOut == True: # If program has been set to guide on fake data for testing,
+				 # variables are set differently than if not testing.  These
+				 # differences are set in this section.
+            self.refName = self.fakeImageDir + 'g' + str(self.currentImage).zfill(4) + '.fits'
+            self.currentImage = self.currentImage + 1
             self.l.logStr('FakeImage\t%s' % str(self.refName), self.logType)
         self.takeImage('image', self.refName,self.expTime)
         refOptions = self.analyze(self.refName)
-        # reference coords are (singluar selection, not robust).  Don't assume the first element is the best.
+        # Future change: reference coords are (singluar selection, not robust). 
+	# Don't assume the first (brightest) element is the best (or allow user
+	# to choose guide star graphically).
         self.ref = refOptions[0]
         return
 
-#does literally everything -- come up with a good explanation for this, but first we need to break it up
- #   def run(self): 
-#	if self.tRef == True or self.ref == None: #if you want a new ref image, this will be True
-#	    self.takeRef()	
-   # return
-
-#does literally everything -- come up with a good explanation for this, but first we need to break it up
+# This function takes only the class as a parameter.  The first
+# check is if a new reference image is needed or not.  If so,
+# it is taken and analyzed, and then the program returns to this
+# function.  So long as not told to quit, the program enters a
+# loop of taking images and sending offsets.  Each time through,
+# a new image is taken (and the program sleeps during this),
+# it is analyzed, the guide star is found using the
+# coordCompare funciton, and offsets are found and returned
+# via the getOffset function.  All of these processes are 
+# written to the logfile each time, and there is a written
+# process for running this same loop on fake images for
+# testing.
     def run(self): 
 	if self.takeRef == True or self.ref == None: #if you want a new ref image, this will be True
 	    self.takeRef(self)	
